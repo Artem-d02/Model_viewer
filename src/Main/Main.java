b42ced3b391b392b9e4d32c0ea32979c7aee9aea
Artem-d02 <artemd2002@yandex.ru>
2023-07-24 21:37:56 +0300

    Added glfw window

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,31 +1,40 @@
 package Main;
 
+import engine.io.Window;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main implements Runnable {
     public Thread game;
+    public static int WIDTH = 1280;
+    public static int HEIGHT = 780;
+    private Window window;
 
     public void start() {
         game = new Thread(this,"game");
         game.start();
     }
 
-    public static void init() {
+    public void init() {
         System.out.println("Initializing game!");
+        window = new Window(WIDTH, HEIGHT, "My window");
+        window.create();
     }
     public void run() {
         init();
-        while(true) {
+        while(!window.shouldClose()) {
             update();
             render();
         }
     }
     private void update() {
-        System.out.println("Update game!");
+        //System.out.println("Update game!");
+        window.update();
     }
 
     private void render() {
-        System.out.println("Rendering game!");
+        //System.out.println("Rendering game!");
+        window.swapBuffers();
     }
 
     public static void main(String[] args) {
---
+++ src/engine/io/Window.java
@@ -0,0 +1,82 @@
+package engine.io;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Window {
+    private int width;
+    private int height;
+    private String title;
+    private long window;
+
+    public Window(int width, int height, String title) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+    public void create() throws IllegalStateException {
+        if (!glfwInit()) {
+            throw new IllegalStateException("Fatal error: GLFW wasn't initialized");
+        }
+
+        window = glfwCreateWindow(width, height, title, 0, 0);
+
+        if (window == 0) {
+            throw new IllegalStateException("Fatal error: window wasn't created");
+        }
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+
+    public void update() {
+        glfwPollEvents();
+    }
+
+    public void swapBuffers() {
+        glfwSwapBuffers(window);
+    }
+
+    public boolean shouldClose() {
+        return glfwWindowShouldClose(window);
+    }
+
+    protected void finalize() throws Throwable {
+        glfwDestroyWindow(window);
+    }
+}
